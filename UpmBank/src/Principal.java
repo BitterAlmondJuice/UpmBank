c0afb40c557316ce88b0192f712ae5a038f469ec
kapza <66725494+BitterAlmondJuice@users.noreply.github.com>
2021-10-25 00:20:03 +0200

    Update and rename Bank.java to Principal.java

---
+++ UpmBank/src/Principal.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        System.out.printf("PlaceHolder seleccion (alta, crear cuenta, deposito, extraccion, transferencia, prestamo, datos)\n");
+        Scanner teclado = new Scanner(System.in);
+        String entrada = teclado.nextLine();
+        switch (entrada) {
+            case "alta":
+
+                String nombre = Alta.nombre();
+                System.out.println("su nombre es " + nombre);
+                String fechaNacimiento = Alta.fechaNacimiento();
+                break;
+
+            case "crear cuenta":
+                System.out.println("PH crear cuenta");
+                break;
+            case "deposito":
+                System.out.println("PH deposito");
+                break;
+            case "extraccion":
+                System.out.println("PH extraccion");
+                break;
+            case "transferencia":
+                System.out.println("PH transferencia");
+                break;
+            case "prestamo":
+                System.out.println("PH prestamo");
+                break;
+            case "datos":
+                System.out.println("PH datos");
+                break;
+            default:
+                System.out.println("PH error_de_entrada");
+                break;
+        }
+    }
+}
