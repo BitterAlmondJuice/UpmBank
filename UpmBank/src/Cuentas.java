e5fb3cf2977930f3caa8f8c3fd89307cb893e531
kapza <66725494+BitterAlmondJuice@users.noreply.github.com>
2021-10-26 11:52:57 +0200

    Update Cuentas.java

--- UpmBank/src/Cuentas.java
+++ UpmBank/src/Cuentas.java
@@ -54,7 +54,7 @@ public class Cuentas {
             c2 = 1;
         }
 
-        SICA = ("" +a1+a2+a3+a4+b1+b2+b3+b4+c1+c2+d1+d2+d3+d4+d5+d6+d7+d8+d9+d10);
+        SICA = (CE+CS+NC);
         return SICA;
     }
 
