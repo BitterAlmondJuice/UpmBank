41221bca2d8b6ad5b915754fd66c6876bd0e6361
kapza <66725494+BitterAlmondJuice@users.noreply.github.com>
2021-10-28 03:31:51 +0200

    Update Principal.java

--- UpmBank/src/Principal.java
+++ UpmBank/src/Principal.java
@@ -2,48 +2,79 @@ import java.util.Scanner;
 
 public class Principal {
 
+    public static void menu() {
+
+        double cuota = 0, intereses = 0.03, ultimaTransferencia = 0, balance = 0, ultimoDeposito = 0, ultimaExtraccion = 0, valorPrestamo = 0;
+        short duracionPrestamo = 0;
+        String numDni;
+        String letraDni;
+        String nombre = "", IBAN = "", correo = "", fechaNacimiento = "", DNI = "", ibanEmisor = "", ibanreceptor = "";
+        Scanner teclado = new Scanner(System.in);
+
+
+        int entrada;
+        do {
+            System.out.print("Seleccione la operacion que desea realizar\nalta (1),  crear cuenta (2),   deposito (3),   extraccion (4),\ntransferencia (5),  prestamo (6),   datos de cuenta(7),\nSalir (8)\n");
+            entrada = teclado.nextInt();
+
+            if (entrada == 1) {
+
+                nombre = Alta.nombre();
+                fechaNacimiento = Alta.fecha();
+                numDni = Alta.numDni();
+                letraDni = Alta.letraDni();
+                correo = Alta.correo();
+                DNI = numDni + letraDni;
+                System.out.println("nombre: " + nombre + "\nfecha de nacimiento: " + fechaNacimiento + "\nDNI: " + numDni + letraDni + "\nemail: " + correo + "\n");
+                System.out.println(fechaNacimiento);
+
+            }
+            else if (entrada == 2) {
+                IBAN = Cuentas.cuenta();
+            }
+            else if (entrada == 3) {
+                System.out.println("PH deposito");
+                ultimoDeposito = Movimientos.deposito(IBAN);
+                balance = balance + ultimoDeposito;
+            }
+            else if (entrada == 4) {
+                System.out.println("PH extraccion");
+                ultimaExtraccion = Movimientos.retirada(IBAN);
+                balance = balance - ultimaExtraccion;
+            }
+            else if (entrada == 5) {
+                System.out.println("PH transferencia");
+                ibanEmisor = Movimientos.ibanEmisor();
+                ibanreceptor = Movimientos.ibanReceptor();
+                ultimaTransferencia = Movimientos.transferencia(ibanEmisor, ibanreceptor);
+                balance = balance - ultimaTransferencia;
+            }
+            else if (entrada == 6) {
+                System.out.println("PH prestamo");
+                valorPrestamo = Prestamo.prestamo();
+                balance = balance + valorPrestamo;
+                duracionPrestamo = (short) Prestamo.meses();
+                cuota = Prestamo.cuota(duracionPrestamo, valorPrestamo, intereses);
+                Prestamo.tablaAmortización(valorPrestamo, duracionPrestamo, cuota, intereses);
+            }
+            else if (entrada == 7) {
+                Datos.datos(nombre, fechaNacimiento, DNI, correo, IBAN, balance, ultimoDeposito, ultimaExtraccion, ultimaTransferencia, ibanEmisor, ibanreceptor, valorPrestamo, duracionPrestamo, cuota, intereses);
+            }
+            else if (entrada == 8) {
+                System.out.println("aDiOs");
+            }
+            else {
+                System.out.println("Error de entrada, por favor, intentelo de nuevo");
+
+            }
+
+        } while (entrada != 8);
+    }
+
     public static void main(String[] args) {
 
-        System.out.printf("PlaceHolder seleccion (alta, crear cuenta, deposito, extraccion, transferencia, prestamo, datos)\n");
-        Scanner teclado = new Scanner(System.in);
-        String entrada = teclado.nextLine();
-        switch (entrada) {
-            case "alta":
-
-                String nombre = Alta.nombre();
-                String fechaNacimiento = Alta.fecha();
-                int numDni = Alta.numDni();
-                String letraDni = Alta.letraDni();
-                String correo = Alta.correo();
-
-                System.out.println("nombre: " + nombre + "\nfecha de nacimiento: " + fechaNacimiento + "\nDNI: " + numDni + letraDni + "\nemail: " + correo);
-
-                break;
-
-            case "crear cuenta":
-                System.out.println("PH crear cuenta");
-                String numCuenta = Cuentas.SICA();
-                System.out.println(numCuenta);
-                break;
-            case "deposito":
-                System.out.println("PH deposito");
-                break;
-            case "extraccion":
-                System.out.println("PH extraccion");
-                break;
-            case "transferencia":
-                System.out.println("PH transferencia");
-                break;
-            case "prestamo":
-                System.out.println("PH prestamo");
-                break;
-            case "datos":
-                System.out.println("PH datos");
-                break;
-            default:
-                System.out.println("PH error_de_entrada");
-                break;
-        }
+        menu();
     }
 
 }
+
